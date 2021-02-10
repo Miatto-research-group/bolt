51aca2c194525e0b37c1b4245176c57c558f16f6
ziofil <miatto@gmail.com>
2021-02-10 10:49:37 -0500

    added time measurement

--- optimizer.py
+++ optimizer.py
@@ -1,23 +1,27 @@
+import time
 import numpy as np
 from tree import Tree
 from numpy_ml.neural_nets.optimizers import Adam
 from states import Requirements
 from tqdm import trange
 from scipy.linalg import expm
+# from expm import expm
 from interferometers import L, dV_dlambdas
 
 class Optimizer:
-    def __init__(self, lr, max_steps):
-        self.max_steps = max_steps
+    def __init__(self, lr, epsilon=1e-5, max_steps=None):
+        self.epsilon = epsilon
+        self.max_steps = max_steps or 10000
         self.opt = Adam(lr=lr)
         self.losses = []
+        self.elapsed = 0.0
     @staticmethod
     def mse(x, y):
         return 0.5*(x-y)**2
 
     
-
     def __call__(self, req:Requirements):
+        tic = time.time()
         lambdas = np.random.normal(size=req.modes**2, scale=1)
         V = expm(L(lambdas))
         self.losses = [1e6]
@@ -41,9 +45,13 @@ class Optimizer:
                 V = expm(L(lambdas))
                 P = ', '.join([f'{p:.4f}' for p in probs])
                 progress.set_description(f"{step}: probs = [{P}], loss = {loss:.4f}")
+                if abs(self.losses[-2] - self.losses[-1]) < self.epsilon:
+                    break
         except KeyboardInterrupt:
-            print('exiting optimization...')
+            print('gracefully stopping optimization...')
         self.losses.pop(0)
+        toc = time.time()
+        self.elapsed = toc-tic
         return V
 
 
