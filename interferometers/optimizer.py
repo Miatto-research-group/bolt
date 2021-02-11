9d9c8fb3e0d53a0c108ff7b2284d410c37991e8b
ziofil <miatto@gmail.com>
2021-02-11 15:24:11 -0500

    saving probs in the optimizer

--- interferometers/optimizer.py
+++ interferometers/optimizer.py
@@ -25,7 +25,9 @@ class Optimizer:
         self.max_steps = max_steps
         self.opt = Adam(lr=lr)
         self.losses = []
+        self.probs = []
         self.elapsed = 0.0
+
     @staticmethod
     def mse(x, y):
         return 0.5*(x-y)**2
@@ -50,12 +52,12 @@ class Optimizer:
             for step in progress:
                 grad_update = 0
                 loss = 0
-                probs = []
+                self.probs = []
                 for io,prob in req.specs.items():
                     tree = Tree(io=io, covariance_matrix=V, grad=True)
                     a, da_dV = tree.amplitude()
                     p = abs(a)**2
-                    probs.append(p)
+                    self.probs.append(p)
                     dL_da = np.conj(a)*(p - prob) - prob**2/a
                     da_dlambdas = np.sum(da_dV * dV_dlambdas(lambdas), axis=(1,2))
                     grad_update += 2*np.real(dL_da * da_dlambdas)
@@ -63,7 +65,7 @@ class Optimizer:
                 self.losses.append(loss)
                 lambdas = self.opt.update(lambdas, grad_update, None, None)
                 V = expm(L(lambdas))
-                P = ', '.join([f'{p:.4f}' for p in probs])
+                P = ', '.join([f'{p:.4f}' for p in self.probs])
                 progress.set_description(f"{step}: probs = [{P}], loss = {loss:.4f}")
                 if abs(self.losses[-2] - self.losses[-1]) < self.epsilon:
                     break
